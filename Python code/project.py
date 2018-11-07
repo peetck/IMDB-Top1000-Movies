2a189a9b6a175e6047e65e561abb871d9b985284
peetck <peekung602@gmail.com>
2018-11-07 23:03:58 +0700

    Update !!@

--- Python code/project.py
+++ Python code/project.py
@@ -11,18 +11,35 @@ def graph1(data):
     revenue = data["Revenue (Millions)"].tolist() # get revenue
     dic_of_totalmoviesales = {} # create dic
     count = 0
+    listed2 = []
     for i in year:
         if i not in dic_of_totalmoviesales and not math.isnan(revenue[count]):
             dic_of_totalmoviesales[i] = revenue[count]
         elif i in dic_of_totalmoviesales and not math.isnan(revenue[count]):
             dic_of_totalmoviesales[i] += revenue[count]
         count += 1
+        listed2.append(i)
     listed = []
     for i in sorted(dic_of_totalmoviesales):
         listed.append(dic_of_totalmoviesales[i])
+
     graph = pygal.Line()
-    graph.title = "กราฟอัตรารายได้การขายภาพยนต์ ทั้งหมด 1000 เรื่อง ในช่วงปี 2006 - 2016 (Million US dollars)"
+    graph.title = "กราฟรายได้ที่ได้จากการขายภาพยนต์ ของหนัง1000เรื่อง (Million US dollars)"
     graph.x_labels = map(str, range(2006, 2017))
-    graph.add("Revenue", listed)
-    graph.render_to_file("Graph of total movies sales in each year.svg")
+    graph.add("", listed)
+    graph.render_to_file("Graph of total revenue of movies sales in each year.svg")
+    movies_of_each_years = {}
+    for i in listed2:
+        if i not in movies_of_each_years:
+            movies_of_each_years[i] = 1
+        else:
+            movies_of_each_years[i] = movies_of_each_years[i] + 1
+    listed3 = []
+    for i in range(2006, 2017):
+        listed3.append(listed2.count(i))
+    graph2 = pygal.Bar()
+    graph2.title = "กราฟอัตราการผลิตภาพยนต์จากทั้งหมด 1000 เรื่อง"
+    graph2.x_labels = map(str, range(2006, 2017))
+    graph2.add("", listed3)
+    graph2.render_to_file("Graph of total movies pro in each year.svg")
 main()
