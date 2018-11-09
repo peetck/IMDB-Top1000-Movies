ffa602b30a5abec7de1354e60a916382a0d6fa87
peetck <peekung602@gmail.com>
2018-11-09 23:54:46 +0700

    Update #

---
+++ Python code/genre_graph.py
@@ -0,0 +1,23 @@
+import pygal
+import pandas as pd
+data = pd.read_csv("../IMDB-Movie-Data.csv")
+def genre_graph(data):
+    genre = data["Genre"].tolist()
+    genre_2 = []
+    dic = {}
+    for i in genre:
+        check = i.split(",")
+        genre_2.append(check[0])
+    for i in genre_2:
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i] += 1
+    create_graph(dic)
+def create_graph(dic):
+    graph = pygal.Pie()
+    graph.title = "ประเภทของหนังที่ได้รับความนิยม ตั้งแต่ปี 2006 - 2016 (1000 เรื่อง)"
+    for i in dic:
+        graph.add(i, dic[i])
+    graph.render_to_file("../Graph/genre_graph.svg")
+genre_graph(data)
