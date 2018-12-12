70a121ef6324ddfb6b4f3f842f2b1998a31eabb7
unknown <peekung602@gmail.com>
2018-12-13 00:40:32 +0700

    UD

--- Python code/project_class.py
+++ Python code/project_class.py
@@ -312,36 +312,51 @@ class project:
         graph.render_to_file("../Graph/genre_price_each_years.svg")
     def runtime(data):
         runtime = data["Runtime (Minutes)"].tolist()
-        runtime = [int(i) for i in runtime]
-        runtime_mean = np.average(runtime)
-        runtime_sd = np.std(runtime)
-        runtime_highest = np.max(runtime)
+        genre = data["Genre"].tolist()
+        dic = {}
+        for i in range(len(runtime)):
+            for j in genre[i].split(","):
+                if j not in dic:
+                    dic[j] = runtime[i]
+                else:
+                    if runtime[i] > dic[j]:
+                        dic[j] = runtime[i]
         graph = pygal.Bar()
         graph.title = "Runtime [IMDB Top 1000 Movies (2006 - 2016)]"
-        graph.x_labels = ["Highest", "Median", "Standard Division"]
-        graph.add("Runtime", [runtime_highest, runtime_mean, runtime_sd])
+        for i in dic:
+            graph.add(i, dic[i])
         graph.render_to_file("../Graph/runtime.svg")
     def vote(data):
         vote = data["Votes"].tolist()
-        vote = [int(i) for i in vote]
-        vote_mean = np.average(vote)
-        vote_sd = np.std(vote)
-        vote_highest = np.max(vote)
+        genre = data["Genre"].tolist()
+        dic = {}
+        for i in range(len(vote)):
+            for j in genre[i].split(","):
+                if j not in dic:
+                    dic[j] = vote[i]
+                else:
+                    dic[j] += vote[i]
         graph = pygal.Bar()
         graph.title = "Vote [IMDB Top 1000 Movies (2006 - 2016)]"
-        graph.x_labels = ["Highest", "Median", "Standard Division"]
-        graph.add("", [vote_highest, vote_mean, vote_sd])
+
+        for i in dic:
+            graph.add(i, dic[i])
         graph.render_to_file("../Graph/vote.svg")
     def rating(data):
         rating = data["Rating"].tolist()
-        rating = [int(i) for i in rating]
-        rating_mean = np.average(rating)
-        rating_sd = np.std(rating)
-        rating_highest = np.max(rating)
+        genre = data["Genre"].tolist()
+        dic = {}
+        for i in range(len(rating)):
+            for j in genre[i].split(","):
+                if j not in dic:
+                    dic[j] = rating[i]
+                else:
+                    dic[j] += rating[i]
         graph = pygal.Bar()
         graph.title = "Rating [IMDB Top 1000 Movies (2006 - 2016)]"
-        graph.x_labels = ["Highest", "Median", "Standard Division"]
-        graph.add("", [rating_highest, rating_mean, rating_sd])
+
+        for i in dic:
+            graph.add(i, dic[i])
         graph.render_to_file("../Graph/rating.svg")
 
     def word(data):
